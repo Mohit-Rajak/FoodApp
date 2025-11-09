8d8d229df757bce3e5e47089e3fd650bd8445538
Mohit Rajak <141298415+Mohit-Rajak@users.noreply.github.com>
2025-11-09 23:02:30 +0530

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -37,7 +37,7 @@ const Section = ({ title, description, isVisibal, setIsVisible }) => {
 						<br></br>
 						<p>{description.subDescription}</p>
 						<p>{description.msg}</p>
-						<p>[Akash Deep]</p>
+						<p>[Mohit Rajak]</p>
 					</div>
 				)}
 			</p>
